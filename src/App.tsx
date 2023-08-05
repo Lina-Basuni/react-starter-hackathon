cc4cb9a36a5a8b9bdb3cd1f9c281d8c1f3cc707f
Lina Bassiouny <lina.bassiouny@visionhealth.co.uk>
2023-08-05 12:30:05 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <Router>
       <>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        <NavBar/>
         <Switch>
           <Route exact path="/">
             <Home />
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import './NavBar.css'; 
+function NavBar() {
+	return (
+		<nav>
+		<ul>
+		  <li>
+			<Link to="/">Home</Link>
+		  </li>
+		  <li>
+			<Link to="/about">About</Link>
+		  </li>
+		</ul>
+	  </nav>
+	);
+  }
+
+  export default NavBar;
